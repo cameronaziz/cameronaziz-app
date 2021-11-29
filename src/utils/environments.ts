376027709b38369e435790ac5f2e999e161918dd
Cameron Aziz <cameronaziz@me.com>
2021-11-28 23:50:57 -0800

    vite

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -4,7 +4,7 @@ const getKeyURL = () => {
   switch (environment) {
     case 'production': return 'https://cameronaziz-keystone-keystone.up.railway.app';
     case 'staging': return 'https://cameronaziz-keystone-staging.up.railway.app';
-    default: return 'http://localhost:3000/';
+    default: return 'http://localhost:5050';
   }
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import postcss from 'rollup-plugin-postcss'
 import sveltePreprocess from 'svelte-preprocess';
 
 const config = defineConfig({
@@ -8,9 +7,6 @@ const config = defineConfig({
     port: 4000,
   },
   plugins: [
-    postcss({
-      plugins: [],
-    }),
     svelte({
       preprocess: sveltePreprocess(),
     })
