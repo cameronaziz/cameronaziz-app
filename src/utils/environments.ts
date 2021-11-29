48ca6988426311c06e45310ecc8243af7a75074f
Cameron Aziz <cameronaziz@me.com>
2021-11-28 23:42:08 -0800

    envs

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -2,8 +2,8 @@ const environment = process.env.NODE_ENV;
 
 const getKeyURL = () => {
   switch (environment) {
-    case 'production': return 'https://cameronaziz-keystone-production.up.railway.app/';
-    case 'staging': return 'https://cameronaziz-keystone-staging.up.railway.app/';
+    case 'production': return 'https://cameronaziz-keystone-keystone.up.railway.app';
+    case 'staging': return 'https://cameronaziz-keystone-staging.up.railway.app';
     default: return 'http://localhost:5050/';
   }
 };
