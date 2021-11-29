5e00bb2b9aca1a67d0b382c4f10c32078da65632
Cameron Aziz <cameronaziz@me.com>
2021-11-28 23:54:53 -0800

    port

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
+const getPort = () => {
+  const port = process.env.PORT
+  if (port) {
+    return Number(port)
+  }
+  return 4000;
+}
+
 const config = defineConfig({
   server: {
-    port: 4000,
+    port: getPort(),
   },
   plugins: [
     svelte({
